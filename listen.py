f1abd59faacb45115fc9670d26db261d36ff70e0
Reiner Hüchting <reiner.huechting@gmx.de>
2021-04-11 11:54:25 +0200

    Testfunktionen direkt zu den Aufgaben geschrieben.

--- listen.py
+++ listen.py
@@ -6,19 +6,6 @@ def maximum(liste):
         return None
     return reduce(lambda x,y : x if x>y else y, liste)
 
-def length(liste):
-    ''' Liefert die Länge der Liste. '''
-    if liste == []:
-        return 0
-    head, *tail = liste
-    return 1 + length(tail)
-
-def without(liste, i):
-    ''' Liefert eine Liste, die alle Elemente außer dem an Stelle `i` enthält.'''
-    if not i in range(0,length(liste)):
-        return liste
-    return liste[:i] + liste[i+1:]
-
 def test_maximum():
     assert(maximum([1,3,6,2,4]) == 6)
     assert(maximum([1,4]) == 4)
@@ -26,12 +13,25 @@ def test_maximum():
     assert(maximum([]) == None)
     print("Tests für maximum() ok.")
 
+def length(liste):
+    ''' Liefert die Länge der Liste. '''
+    if liste == []:
+        return 0
+    head, *tail = liste
+    return 1 + length(tail)
+
 def test_length():
     assert(length([1,3,6,2,4]) == 5)
     assert(length([1,4]) == 2)
     assert(length([8]) == 1)
     assert(length([]) == 0)
     print("Tests für length() ok.")
+
+def without(liste, i):
+    ''' Liefert eine Liste, die alle Elemente außer dem an Stelle `i` enthält.'''
+    if not i in range(0,length(liste)):
+        return liste
+    return liste[:i] + liste[i+1:]
 
 def test_without():
     assert(without([1,3,6,2,4],2) == [1,3,2,4])
